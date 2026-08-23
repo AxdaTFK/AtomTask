ece68e624253ebf90a5caa457081fd4ab564cf4a
AxdaTFK <axldug5@gmail.com>
2026-08-22 19:36:10 -0500

    Atomo finalizado

--- Assets/Scripts/ElectronOrbit.cs
+++ Assets/Scripts/ElectronOrbit.cs
@@ -6,25 +6,46 @@ public class ElectronOrbit : MonoBehaviour
 
     public float radiusX = 5f;
     public float radiusY = 2.5f;
-    
-    public float speed = 300f;
+
+    public float speed = 100f;
 
     public float startAngle = 0f;
+
+    public float rotation = 0f;
+
+    public SpriteRenderer sr;
+    public int nucleusOrder = 10;
+    public int frontOffset = 1;
+    public int backOffset = 1;
 
     private float angle;
 
     void Start()
     {
         angle = startAngle;
+        if (sr == null) sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         angle += speed * Time.deltaTime;
 
-        float x = center.position.x + radiusX * Mathf.Cos(angle * Mathf.Deg2Rad);
-        float y = center.position.y + radiusY * Mathf.Sin(angle * Mathf.Deg2Rad);
+        float elipX = radiusX * Mathf.Cos(angle * Mathf.Deg2Rad);
+        float elipY = radiusY * Mathf.Sin(angle * Mathf.Deg2Rad);
 
-        transform.position = new Vector2(x, y);
+        float rotRad = rotation * Mathf.Deg2Rad;
+
+        float x = elipX * Mathf.Cos(rotRad) - elipY * Mathf.Sin(rotRad);
+        float y = elipX * Mathf.Sin(rotRad) + elipY * Mathf.Cos(rotRad);
+
+        transform.position = new Vector2(center.position.x + x, center.position.y + y);
+        if (Mathf.Sin(angle * Mathf.Deg2Rad) > 0f)
+        {
+            sr.sortingOrder = nucleusOrder - backOffset;
+        }
+        else
+        {
+            sr.sortingOrder = nucleusOrder + frontOffset;
+        }
     }
 }
